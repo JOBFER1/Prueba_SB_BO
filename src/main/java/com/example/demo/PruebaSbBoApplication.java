1b895bf1c547625527147327b9f070d54282052b
JOBFER1 <jobfer@outlook.es>
2023-06-01 18:33:15 +0200

    Commit inicial

---
+++ src/main/java/com/example/demo/PruebaSbBoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSbBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSbBoApplication.class, args);
+		
+		System.out.println("Hola Mundo!!");
+	}
+
+}
---
+++ src/test/java/com/example/demo/PruebaSbBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSbBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
